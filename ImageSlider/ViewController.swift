8bc1d486590fbd27ae36f8bf98c7138dacea982b
juncollin <juncollin@gmail.com>
2018-05-22 19:07:10 +0900

    Initial Commit

---
+++ ImageSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageSlider
+//
+//  Created by 有本淳吾 on 2018/05/22.
+//  Copyright © 2018年 有本淳吾. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
